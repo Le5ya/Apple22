79ded4b8284efa364af8eed0e576582ba5f39ef9
Le5ya <liliatolstyh@yandex.ru>
2022-09-18 12:52:17 +0300

    timer

--- scripts/index.js
+++ scripts/index.js
@@ -90,3 +90,48 @@ const showPrice = (currency = 'USD') => {
 
 }
 showPrice();
+
+const declOfNum = (n, titles) => titles[n % 10 === 1 && n % 100 !== 11 ?
+0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2]
+const timer = (deadline) => {
+	const unitDays = document.querySelector('.timer__unit-day');
+	const unitHours = document.querySelector('.timer__unit-hour');
+	const unitMinutes = document.querySelector('.timer__unit-min');
+
+	const daysDes = document.querySelector('.timer__des-day');
+	const hoursDes = document.querySelector('.timer__des-hour');
+	const minutesDes = document.querySelector('.timer__des-min');
+
+	const getTimeRemaining = () => {
+		const dateStop = new Date(deadline).getTime();
+		const dateNow = Date.now();
+		const timeRemaining = dateStop - dateNow;
+
+		const unitMinutes = Math.floor(timeRemaining / 1000 / 60 % 60);
+		const unitHours = Math.floor(timeRemaining / 1000 / 60 / 60 % 24);
+		const unitDays = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
+
+		return { timeRemaining, unitMinutes, unitHours, unitDays };
+	};
+
+	const start = () => {
+		const timer = getTimeRemaining();
+
+		unitDays.textContent = timer.unitDays;
+		unitHours.textContent = timer.unitHours;
+		unitMinutes.textContent = timer.unitMinutes;
+
+		daysDes.textContent = declOfNum(unitDays, ['день', 'дня', 'дней']);
+		hoursDes.textContent = declOfNum(unitHours, ['час', 'часа', 'часов']);
+		minutesDes.textContent = declOfNum(unitMinutes, ['минута', 'минуты', 'минут']);
+
+		const timerId = setTimeout(start, 60000);
+	}
+	start();
+};
+timer('2023/10/07 20:00');
+
+
+
+
+
